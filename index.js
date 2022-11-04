770eb6c7bf64bb1b1f535e26f661cdda8514a29b
Stephen Blott <stephen.blott@dcu.ie>
2022-11-04 10:10:43 +0000

    Preparing setup for ca282/2023.

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
 
 /**
- * Example of a different API
+ * Example of a different API.
  *
  * You can copy the ideas here to write your own API.
  *
- * This is an example of a random number generator.  The HTTP endpoints are:
+ * This is an example of a random number generator.
  *
- * POST /test/maximum
+ * The HTTP endpoints are:
  *
- *    The data is a JSON-encoded maximum for the random number generator, and
- *    this call sets the maximum to that value.
+ *   POST /test/maximum
  *
- * GET /test/maximum
+ *      The data is a JSON-encoded maximum for the random number generator, and
+ *      this call sets the maximum to that value.
  *
- *    Get the current maximum..
+ *   GET /test/maximum
  *
- * GET /test/random
+ *      Get the current maximum.
  *
- *    Return a random number.
+ *   GET /test/random
+ *
+ *      Return a random number.
  */
 
 var maxRandom = 10;
@@ -36,45 +38,43 @@ app.use(bodyParser.json());
 // req.body (see above).
 //
 app.post("/test/maximum", function(req, res) {
-   if ( req.body
-        && req.body.hasOwnProperty("maximum")
-        && Number.isInteger(req.body.maximum)
-        && 0 < req.body.maximum ) {
-      maxRandom = req.body.maximum;
-      console.log("server: setting maximum to", maxRandom);
-      res.setHeader("Content-Type", "text/plain");
-      res.send(`server... ok, setting maximum: ${maxRandom}\n`);
-   }
-   else {
-      res.status(400).send("bad request (invalid maximum value)\n");
-   }
+  if ( req.body
+       && req.body.hasOwnProperty("maximum")
+       && Number.isInteger(req.body.maximum)
+       && 0 < req.body.maximum ) {
+    maxRandom = req.body.maximum;
+    console.log("server: setting maximum to", maxRandom);
+    res.setHeader("Content-Type", "text/plain");
+    res.send(`server... ok, setting maximum: ${maxRandom}\n`);
+  }
+  else {
+    res.status(400).send("bad request (invalid maximum value)\n");
+  }
 });
 
 app.get("/test/maximum", function(req, res) {
-   console.log("server: getting maximum", maxRandom);
-   res.setHeader("Content-Type", "application/json");
-   response = { maximum: maxRandom };
-   res.send(JSON.stringify(response));
+  console.log("server: getting maximum", maxRandom);
+  res.setHeader("Content-Type", "application/json");
+  response = { maximum: maxRandom };
+  res.send(JSON.stringify(response));
 });
 
 // Example GET request (including handling an error case).
 //
 app.get("/test/random", function(req, res) {
-   if (maxRandom <= 0) {
-      res.status(400).send("bad request (maximum not set)\n");
-   }
-   else {
-      var r = Math.floor(Math.random() * maxRandom);
-      console.log("server... sending random:", r);
-      res.setHeader("Content-Type", "application/json");
-      res.send(JSON.stringify(r) + "\n");
-   }
+  if (maxRandom <= 0) {
+    res.status(400).send("bad request (maximum not set)\n");
+  }
+  else {
+    var r = Math.floor(Math.random() * maxRandom);
+    console.log("server... sending random:", r);
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify(r) + "\n");
+  }
 });
 
 /**
  * Serve static files from the directory ./static.
- *
- * Just "index.html" is served from here.
  */
 
 app.use(express.static('./static'));
@@ -94,12 +94,29 @@ app.use(express.static('./static'));
  *
  *   - For the /push request, the JSON body will already have been parsed by
  *     the body parser above.  You don't need to do anything special.  Just
- *     validate req.body.values, much like the POST request above.
+ *     validate req.body.values, much like the POST example above.
  *
- * The details of the required API are defined in static/index.(ascii|html).
+ * The details of the required API are defined in static/README.md.
  */
 
+// This is your stack; it's a Javascript array.
+//
 var stack = [];
+
+app.post("/push", function(req, res) {
+  // This seems like the obvious place to start.
+});
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * (You shouldn't have to change anything ABOVE here.)
@@ -129,7 +146,7 @@ var stack = [];
  */
 
 app.listen(port, function() {
-   console.log("listening on port", port, "...");
+  console.log("listening on port", port, "...");
 });
 
 module.exports = app
